2ac868f47cb9f0b9472f7135938dcb6b3f135fbe
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2023-05-22 10:40:59 -0400

    finished a4

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -1,4 +1,6 @@
 /* TODO: Create a subclass of Trader named DrivableTrader
+
+
  * This class should be identical to Trader, except that it takes
  * only Drivable objects in its inventory, wishlist, etc.
  *
@@ -11,3 +13,22 @@
  */
 
 import java.util.List;
+
+public class DrivableTrader extends Trader {
+
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist,
+                          int money) {
+        super(inventory, wishlist, money);
+    }
+    public DrivableTrader(int money) {
+        super(money);
+    }
+
+    public int getSellingPrice(Drivable object) {
+        if (object instanceof Tradable) {
+            return ((Tradable) object).getPrice() + object.getMaxSpeed();
+        } else {
+            return Tradable.MISSING_PRICE;
+        }
+    }
+}
--- src/main/java/Trader.java
+++ src/main/java/Trader.java
@@ -39,6 +39,11 @@ public class Trader<T> {
      *       representing the Trader's money. Give the Trader
      *       empty ArrayLists for their inventory and wishlist.
      */
+    public Trader(int money) {
+        this.inventory = new ArrayList<>();
+        this.wishlist = new ArrayList<>();
+        this.money = money;
+    }
 
 
 
@@ -48,7 +53,9 @@ public class Trader<T> {
      *       object of type T and adds it to this Trader's wishlist.
      */
 
-
+    public void addToWishlist(T item) {
+        this.wishlist.add(item);
+    }
 
 
 
@@ -59,7 +66,13 @@ public class Trader<T> {
      *
      *       We will call this in exchangeMoney().
      */
-
+    public int getSellingPrice(T object) {
+        if (object instanceof Tradable) {
+            return ((Tradable) object).getPrice();
+        } else {
+            return Tradable.MISSING_PRICE;
+        }
+    }
 
 
 
